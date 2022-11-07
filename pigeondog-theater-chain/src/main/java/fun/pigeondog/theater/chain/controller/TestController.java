7d0f5c90ea2882ecdb1fd2d63cd06a57381ab20f
yzguo <yzguoc@outlook.com>
2022-11-07 21:05:37 +0800

    swagger

--- pigeondog-theater-chain/src/main/java/fun/pigeondog/theater/chain/controller/TestController.java
+++ pigeondog-theater-chain/src/main/java/fun/pigeondog/theater/chain/controller/TestController.java
@@ -2,6 +2,8 @@ package fun.pigeondog.theater.chain.controller;
 
 import com.alibaba.fastjson2.JSONObject;
 import fun.pigeondog.common.annotation.CommonResultAdvice;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @author yzguo
  * @date 2022/10/16 13:34
  */
+@Api(tags = "测试类")
 @RestController
 @CommonResultAdvice
 @RequestMapping("/test")
 public class TestController {
 
+    @ApiOperation("测试获取字符串")
     @GetMapping("/str")
     public JSONObject test() {
         JSONObject jsonObject = new JSONObject();
